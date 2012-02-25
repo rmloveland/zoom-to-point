b36878c382651e51a05fe81e981495434dc35c9e
Rich Loveland <loveland.richard@gmail.com>
2012-02-25 10:27:52 -0500

    Added ``togglability" function for ease of use.

--- zoom-to-point.el
+++ zoom-to-point.el
@@ -18,53 +18,36 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;;  * ZOOM-TO-POINT
-
-;; This is not an Emacs ``mode" so much as two convenient function
-;; definitions and a slightly less convenient variable declaration (e.g.,
-;; =zoom-to-point=, =unzoom=, and =*zoom-level*=).
-
-;; That said, the functions at least do pretty much what they say:
-;; namely, they zoom towards and away from the point using the specified ``level" of
-;; zoom you require, with the now-larger or -smaller text centered in the
-;; buffer.
-
-;; Specify the zoom-level in the parameter =*default-zoom-level*=. For now, type
-
-;; #+BEGIN_SRC emacs-lisp
-;;   (defvar *default-zoom-level* 3)
-;; #+END_SRC
-
-;; into the =*scratch*= buffer and evaluate it.
-
-;; Then zoom in on whatever you're aimed at with
-
-;; #+BEGIN_EXAMPLE
-;; M-x zoom-to-point
-;; #+END_EXAMPLE
-
-;; To return to your previous code- or text-viewing experience, use
-
-;; #+BEGIN_EXAMPLE
-;; M-x unzoom
-;; #+END_EXAMPLE
-
-;; This interface is clunky and represents a minimal first working
-;; version. Expect changes, or better yet, contribute some!
-;; This is not an Emacs mode so much as two function definitions and
+;;
+;;  See the included file README.org for details and usage
+;;  information. For now the code lives at
+;;  https://github.com/rmloveland/zoom-to-point
 
 (defvar *default-zoom-level* 3)
+
+(defun zoom-toggle ()
+  (interactive)
+  (save-excursion
+    (if (zoomed-p)
+	(unzoom)
+      (zoom-to-point))))
 
 (defun zoom-to-point ()
   (interactive)
   (save-excursion
     (text-scale-increase *default-zoom-level*)
-    (recenter-top-bottom)))
+    (recenter)))
 
 (defun unzoom ()
   (interactive)
   (save-excursion
-    (text-scale-decrease *default-zoom-level*)
-    (recenter-top-bottom)))
+    (text-scale-set 0)
+    (recenter)))
+
+(defun zoomed-p ()
+  (interactive)
+  (if (= text-scale-mode-amount 0)
+      nil
+    t))
 
 (provide 'zoom-to-point)
