38b11fe8f110eae4c8c7e1a0b40e05f9e58c7f36
Rich Loveland <r@rmloveland.com>
2013-12-16 08:07:40 -0500

    Updated function docstrings (ran checkdoc); renamed internal-only functions.

--- zoom-to-point.el
+++ zoom-to-point.el
@@ -1,8 +1,10 @@
-;; Copyright (C) 2012 Rich Loveland
+;;; zoom-to-point.el --- Zoom in or out on the line at point.
 
-;; Author: Rich Loveland  <loveland.richard@gmail.com>
-;; Keywords: automation, convenience
-;; Version: 0.1
+;; Copyright (C) 2012, 2013 Rich Loveland
+
+;; Author: Rich Loveland  <r@rmloveland.com>
+;; Keywords: convenience, text size, zoom
+;; Version: 0.2
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -19,35 +21,52 @@
 
 ;;; Commentary:
 ;;
-;;  See the included file README.org for details and usage
-;;  information. For now the code lives at
-;;  https://github.com/rmloveland/zoom-to-point
+;;  Exposes a command, `zoom-toggle', which will increase or decrease
+;;  the size of the text at point, and a variable,
+;;  `*default-zoom-level*', which determines the change in size.
 
-(defvar *default-zoom-level* 3)
+;;; Code:
+
+(defvar *default-zoom-level* 3
+  "Defines how much the `zoom-toggle' command will increase the text scale.")
+
+;; This variable is buffer-local, and isn't always initialized; this
+;; causes an error.  We initialize it here to avoid causing errors.
+;; So far it's worked well.
+(setq text-scale-mode-amount 0)
 
 (defun zoom-toggle ()
+  "Zoom in or out on the line at point.
+More precisely, increase or decrease the size of the text by the
+amount defined in `*default-zoom-level*', and centers that line
+in the window."
   (interactive)
-  (if (zoomed-p)
-      (unzoom)
-    (zoom-to-point)))
+  (if (zoom--zoomed-p)
+      (zoom--unzoom)
+    (zoom--zoom-to-point)))
 
-(defun zoom-do (thunk)
-  (interactive)
+(defun zoom--do (thunk)
+  "Call THUNK, and then recenter point."
   (save-excursion
     (funcall thunk)
     (recenter)))
 
-(defun zoom-to-point ()
-  (zoom-do (lambda ()
+(defun zoom--zoom-to-point ()
+  "Zoom in on point."
+  (zoom--do (lambda ()
 	     (text-scale-increase *default-zoom-level*))))
 
-(defun unzoom ()
-  (zoom-do (lambda ()
+(defun zoom--unzoom ()
+  "Zoom out from point and return to the default text scale."
+  (zoom--do (lambda ()
     (text-scale-set 0))))
 
-(defun zoomed-p ()
+(defun zoom--zoomed-p ()
+  "Is the buffer text larger than the default?"
   (if (= text-scale-mode-amount 0)
       nil
     t))
 
 (provide 'zoom-to-point)
+
+;;; zoom-to-point.el ends here
